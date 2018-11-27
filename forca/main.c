4abc94fc1c8f4e90fdd21d86bb53b8dc30647b21
notexza <josepedro1997@hotmail.com>
2018-11-27 15:17:20 +0000

    login (unfinished)

--- forca/main.c
+++ forca/main.c
@@ -12,11 +12,11 @@ titulo() {
     printf("\\____/ \\___/ \\__, |\\___/   \\__,_|\\__,_| \\_| \\___/|_|  \\___\\__,_|\n");
     printf("             __ / |                                             \n");
     printf("             |___/                                              \n");
-
-    startMenu();
 }
 
 startMenu() {
+    titulo();
+
     int opc;
 
     printf("1 - Entrar\n");
@@ -25,10 +25,35 @@ startMenu() {
     printf("\n");
     printf("Escolha uma opção: ");
     scanf("%d", &opc);
+
+    switch(opc) {
+        case 1:
+            entrarMenu();
+            break;
+    }
+}
+
+entrarMenu() {
+    system("cls");
+    titulo();
+
+    char username[10], password[20], ch;
+    int i=0;
+
+    printf("\nIntroduza o seu nome: ");
+    scanf("%s", &username[10]);
+    printf("\nIntroduza a sua palavra-passe: ");
+
+    for(i=0;i<19;i++) {
+        ch = getch();
+        password[i] = ch;
+        ch = '*' ;
+        printf("%c",ch);
+    }
 }
 
 main()
 {
     setlocale(LC_ALL, "");
-    titulo();
+    startMenu();
 }
