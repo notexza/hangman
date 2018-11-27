b0647454b79fe152cc5101fe1d95029ac8998943
notexza <josepedro1997@hotmail.com>
2018-11-27 16:37:14 +0000

    register

--- forca/main.c
+++ forca/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <conio.h>
+
 
 titulo() {
     // TITULO BLYAT
@@ -30,20 +32,24 @@ startMenu() {
         case 1:
             entrarMenu();
             break;
+        case 2:
+            registarMenu();
+            break;
     }
 }
 
-entrarMenu() {
+
+registarMenu() {
     system("cls");
     titulo();
 
-    char username[10], password[20], ch;
+    char username[10], password[20], password2[20], ch, ch1;
     int i=0;
 
     printf("\nIntroduza o seu nome: ");
-    scanf("%s", &username[10]);
-    printf("\nIntroduza a sua palavra-passe: ");
+    scanf("%s", username);
 
+    printf("\nIntroduza a sua palavra-passe: ");
     for(i=0;i<19;i++) {
         ch = getch();
         password[i] = ch;
@@ -52,6 +58,50 @@ entrarMenu() {
         }
         ch = '*' ;
         printf("%c",ch);
+    }
+
+    FILE *f = fopen("userData.txt", "ab+");
+    fprintf(f, "%s %s", username, password);
+    fclose(f);
+
+}
+
+entrarMenu(char username[10], char password[20]) {
+    system("cls");
+    titulo();
+
+    char username2[10], password2[20], ch;
+    int i=0;
+
+    printf("\nIntroduza o seu nome: ");
+    scanf("%s", username2);
+    printf("\nIntroduza a sua palavra-passe: ");
+
+    for(i=0;i<19;i++) {
+        ch = getch();
+        password2[i] = ch;
+        if(ch==13) {
+            break;
+        }
+        ch = '*' ;
+        printf("%c",ch);
+    }
+
+    FILE *file;
+    file = fopen("userData.txt", "r");
+    if(file) {
+        char line[128];
+        while(fgets(line, sizeof line, file)) {
+                if(sscanf(line, "%s %s", username2, password2) != 2) {
+                    printf("\nUsername e palavra-passe corretos!\n");
+                    system("cls");
+                    startMenu();
+                } else { // FIX THIS LINE, DOESNT WORK
+                    printf("\nUsername e palavra-passe incorretos!\n");
+                    break;
+                }
+        }
+        fclose(file);
     }
 }
 
