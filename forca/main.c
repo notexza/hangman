bd4f99e0bf78cd1f438f2e45a787a027011fb6e8
notexza <josepedro1997@hotmail.com>
2018-11-27 15:01:09 +0000

    menu choices

--- forca/main.c
+++ forca/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int main()
-{
+titulo() {
     // TITULO BLYAT
     printf("   ___                         _        ______\n");
     printf("  |_  |                       | |       |  ___|\n");
@@ -12,4 +12,23 @@ int main()
     printf("\\____/ \\___/ \\__, |\\___/   \\__,_|\\__,_| \\_| \\___/|_|  \\___\\__,_|\n");
     printf("             __ / |                                             \n");
     printf("             |___/                                              \n");
+
+    startMenu();
+}
+
+startMenu() {
+    int opc;
+
+    printf("1 - Entrar\n");
+    printf("2 - Registar\n");
+    printf("3 - Sair\n");
+    printf("\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opc);
+}
+
+main()
+{
+    setlocale(LC_ALL, "");
+    titulo();
 }
